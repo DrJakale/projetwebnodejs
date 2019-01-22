a1c069486833dc5dd652d18fa74de2375f4bd1be
Ivan Chaban <ivan.chaban@viacesi.fr>
2019-01-22 11:46:58 +0100

    API fonctionnelle pour "Users"

--- app.js
+++ app.js
@@ -1,79 +1,115 @@
-const express = require('express')
+﻿const express = require('express')
 const morgan = require('morgan')
 const mysql = require('mysql')
 const bodyParser = require('body-parser')
 const app = express()
 
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(morgan('short'))
-
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'workshop',
-  password: 'workshop',
-  database: 'users'
+app.listen(8080, () =>{
+  console.log("NODE JS API IS RUNNING ON PORT 8080!")
 })
 
-app.get("/", (req, res) =>{
-  console.log("Responding to route")
-  res.send("Hello from route")
-})
-
-app.get("api/user/:id", (req, res) => {
-  const connection = mysql.createConnection({
-    host: 'localhost',
+function getSQL(){
+  return mysql.createConnection({
+    host: 'chabanvpn.ovh',
+    port: '3306',
     user: 'workshop',
     password: 'workshop',
     database: 'users'
   })
+}
 
-  const queryString = "SELECT * FROM users WHERE id = ?"
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(morgan('short'))
+
+app.get("/", (req, res) => {
+  console.log("Responding to ping request")
+  res.send("NODE.JS SERVER UP AND RUNNING")
+})
+
+app.get("/api/user/:id", (req, res) => {
+
+  const connection = getSQL()
+
+  var queryString = "SELECT * FROM users WHERE ID = ?"
   connection.query(queryString, [req.params.id], (err, rows, fields) => {
     if(err){
-      console.log("Erreur")
+      console.log("Erreur: " + err)
       res.sendStatus(500)
       return
     }
-    console.log("OK SQL")
+    console.log("User ID: "+req.params.id+" found")
     res.json(rows)
   })
 
 })
 
-app.get("/api/users", (req, res) =>{
+app.get("/api/users", (req, res) => {
 
+  const connection = getSQL()
 
-  const queryString = "SELECT * FROM users"
+  var queryString = "SELECT * FROM users"
   connection.query(queryString, (err, rows, fields) => {
     if(err){
       console.log("Erreur: " + err)
       res.sendStatus(500)
       return
     }
-    console.log("OK SQL")
+    console.log("User list published")
     res.json(rows)
   })
 })
 
 
 //POST API
-  app.post("/api/user", (req , res) => {
-    var query = "INSERT INTO users VALUES (" +req.body.Firstname+ ","+req.body.Surname+","+req.body.Username+","+req.body.password+","+req.body.Email+","+req.body.Localization+")";
-    connection.query(query);
-});
+app.post("/api/user", (req , res) => {
+
+    const connection = getSQL()
+
+    var queryString = "INSERT INTO users (Firstname, Surname, Username, password, Email, Localization) VALUES (?,?,?,?,?,?)"
+    connection.query(queryString, [req.body.Firstname, req.body.Surname, req.body.Username, req.body.password, req.body.Email, req.body.Localization], (err, results) => {
+      if(err){
+        console.log("Echec de l'insertion")
+        res.sendStatus(500)
+        return
+      }
+      console.log("Success with inserting with ID: ", results.insertId)
+      res.send("Success")
+      //res.end()
+    })
+})
+
+// DELETE API
+ app.delete("/api/user/:id", function(req , res){
+
+      const connection = getSQL()
+
+      var queryString = "DELETE FROM users WHERE ID = ?";
+      connection.query(queryString, [req.params.id], (err, rows, fields) => {
+        if(err){
+          console.log("Erreur: " + err)
+          res.sendStatus(500)
+          return
+        }
+        console.log("Success with deleting ID: ", req.params.id)
+        res.send("Success")
+        //res.end()
+      })
+})
 
 //PUT API
  app.put("/api/user/:id", function(req , res){
-                var query = "UPDATE [user] SET Name= " + req.body.Name  +  " , Email=  " + req.body.Email + "  WHERE Id= " + req.params.id;
-                executeQuery (res, query);
-});
 
-// DELETE API
- app.delete("/api/user /:id", function(req , res){
-                var query = "DELETE FROM [user] WHERE Id=" + req.params.id;
-                executeQuery (res, query);
-});
+        const connection = getSQL()
 
-app.listen(8080, () => {
-  console.log("OK: port 8080")
-})
+        var queryString = "UPDATE users SET Firstname = ? , Surname = ? , Username = ? , password = ? , Email = ? , Localization = ? WHERE (ID = ?)"
+        connection.query(queryString, [req.body.Firstname, req.body.Surname, req.body.Username, req.body.password, req.body.Email, req.body.Localization, req.params.id], (err, results) => {
+          if(err){
+            console.log("Echec de la modification")
+            res.sendStatus(500)
+            return
+          }
+          console.log("Success with modifying ID: ", req.params.id)
+          res.send("Success")
+          //res.end()
+          })
+});
